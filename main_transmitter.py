1bf6d2650aeea17f3155e51f415f48222f6fbb04
DaanDelabie <44842230+DaanDelabie@users.noreply.github.com>
2020-05-20 00:09:03 +0200

    Add files via upload

--- main_transmitter.py
+++ main_transmitter.py
@@ -44,78 +44,83 @@ with open('ConfigTransmitter.json') as json_file:
 
 
 def main():
-    #########################################################################################
-    #                               Audio to Bin setup
-    #########################################################################################
-    print("================================================")
-    print("Audio file to bin file setup")
-    print("================================================")
-    #######################################################
-    #               Block Audio to bin
-    #######################################################
-    print("Setting up audio to bin")
-    # Audio file to bin file setup
-    util.setup_block_audio_to_bin(hold_data_tx, config)
+    diff_fac = (config['aantal_Users'] * config['difference_Factor']) + config['extra_DC']
+    if diff_fac > 9:
+        print("ERROR: No matching difference_Factor and extra_DC values. Maximum out going voltage > 9 V")
+        print("MAX voltage = (#users * difference_Factor) + extra_DC = ", diff_fac)
+    else:
+        #########################################################################################
+        #                               Audio to Bin setup
+        #########################################################################################
+        print("================================================")
+        print("Audio file to bin file setup")
+        print("================================================")
+        #######################################################
+        #               Block Audio to bin
+        #######################################################
+        print("Setting up audio to bin")
+        # Audio file to bin file setup
+        util.setup_block_audio_to_bin(hold_data_tx, config)
 
-    #########################################################################################
-    #                                  Thread setup
-    #########################################################################################
-    print("================================================")
-    print("Setting up all the threads from the TRANSMITTER")
-    print("================================================")
-    ########################################################
-    #                  Block Pack Data
-    ########################################################
-    print("Creating the pack data threads")
-    # Create the pack data threads
-    pack_data_user1_thread = BlockPackDataThread(name='pack_data_user1')
-    pack_data_user2_thread = BlockPackDataThread(name='pack_data_user2')
-    ########################################################
-    #                 Block CDMA Encoder
-    ########################################################
-    print("Creating the CDMA encoder thread")
-    # Create the CDMA encoder thread
-    CDMA_encoder_thread = BlockCDMAencoderThread(name='cdma_encoder_block')
-    ########################################################
-    #                  Block Transmitter
-    ########################################################
-    print("Creating the transmitter thread")
-    # Create the transmitter thread
-    transmitter_thread = BlockDAQTransmitterThread(name='transmitter_block')
+        #########################################################################################
+        #                                  Thread setup
+        #########################################################################################
+        print("================================================")
+        print("Setting up all the threads from the TRANSMITTER")
+        print("================================================")
+        ########################################################
+        #                  Block Pack Data
+        ########################################################
+        print("Creating the pack data threads")
+        # Create the pack data threads
+        pack_data_user1_thread = BlockPackDataThread(name='pack_data_user1')
+        pack_data_user2_thread = BlockPackDataThread(name='pack_data_user2')
+        ########################################################
+        #                 Block CDMA Encoder
+        ########################################################
+        print("Creating the CDMA encoder thread")
+        # Create the CDMA encoder thread
+        CDMA_encoder_thread = BlockCDMAencoderThread(name='cdma_encoder_block')
+        ########################################################
+        #                  Block Transmitter
+        ########################################################
+        print("Creating the transmitter thread")
+        # Create the transmitter thread
+        transmitter_thread = BlockDAQTransmitterThread(name='transmitter_block')
 
-    #########################################################################################
-    #                                  Start Thread
-    #########################################################################################
-    print("================================================")
-    print("Starting all threads from the Transmitter")
-    print("================================================")
-    ########################################################
-    #                  Block Pack Data
-    ########################################################
-    print("Starting the pack data threads")
-    # Start the pack data threads
-    pack_data_user1_thread.start()
-    pack_data_user2_thread.start()
-    ########################################################
-    #                 Block CDMA Encoder
-    ########################################################
-    print("Starting the CDMA encoder thread")
-    # Start the CDMA encoder thread
-    CDMA_encoder_thread.start()
-    ########################################################
-    #                Wait 20s for setting
-    #                    up receiver
-    ########################################################
-    print("Waiting 20 seconds to set up receiver")
-    # Sleep for 1 seconds to set up receiver
-    time.sleep(1)
-    print("Done waiting!!!")
-    ########################################################
-    #                  Block Transmitter
-    ########################################################
-    print("Starting the transmitter thread")
-    # Start the transmitter thread
-    transmitter_thread.start()
+        #########################################################################################
+        #                                  Start Thread
+        #########################################################################################
+        print("================================================")
+        print("Starting all threads from the Transmitter")
+        print("================================================")
+        ########################################################
+        #                  Block Pack Data
+        ########################################################
+        print("Starting the pack data threads")
+        # Start the pack data threads
+        pack_data_user1_thread.start()
+        pack_data_user2_thread.start()
+        ########################################################
+        #                 Block CDMA Encoder
+        ########################################################
+        print("Starting the CDMA encoder thread")
+        # Start the CDMA encoder thread
+        CDMA_encoder_thread.start()
+        ########################################################
+        #                Wait 20s for setting
+        #                    up receiver
+        ########################################################
+        print("Waiting 20 seconds to set up receiver")
+        # Sleep for 1 seconds to set up receiver
+        time.sleep(1)
+        print("Done waiting!!!")
+        ########################################################
+        #                  Block Transmitter
+        ########################################################
+        print("Starting the transmitter thread")
+        # Start the transmitter thread
+        transmitter_thread.start()
 
 
 if __name__ == '__main__':
